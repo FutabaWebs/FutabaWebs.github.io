fcb53ef26efaf199afadcac1573caa118b5f9f43
Futaba_Webs <39473112+FutabaWebs@users.noreply.github.com>
2025-09-29 02:01:06 -0400

    Add files via upload

---
+++ TerminalMode/js/test.js
@@ -0,0 +1,8 @@
+//This is a test function that will resize an elementin the main banne 
+
+function test()
+{
+    alert('testing');
+    document.getElementsByClassName('FutabaLogo').style = "backgronud-color: red;";
+
+}
