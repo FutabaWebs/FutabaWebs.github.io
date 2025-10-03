5de9caa03aab57229bdb682b1f6a8b662167ab48
Futaba_Webs <39473112+FutabaWebs@users.noreply.github.com>
2025-10-02 22:01:28 -0400

    Add files via upload

---
+++ Legacy Functions.js
@@ -0,0 +1,241 @@
+//Search Mode
+function ModalLoad()
+{    
+    document.getElementById("testq").innerHTML = '<h2 style = "color: rgb(114, 3, 193)">Futaba_Webs 🕸🕸🎃🕸🕸</h2> <p> Run the "Help" Command</p> <p> "Shift-Alt-P" (Opens Search Mode)</p> </br> <ul id="myUL" style = "list-style-type: none;"> <li><a href="https://futabawebs.github.io/Articles/Is_TCM_Still_Credible" style = "text-decoration: none; color: #D43F52; font-weight: bold;">🎃 Is TCM Security Still Credible? <img src="Images/futaba 6.jpg" class="img-responsive" alt="portfolio img"></a></li> </br> <li><a href="https://futabawebs.github.io/Articles/HowToBuildYourPersonalBrand" style = "text-decoration: none; color: #D43F52; font-weight: bold;">🎃 How to Build Your Personal Brand? <img src="Images/futaba 5.jpg" class="img-responsive" alt="portfolio img"></a> </li> </br> <li><a href="https://futabawebs.github.io/Articles/WomensHistoryMonth2025"style = "text-decoration: none; color: #D43F52; font-weight: bold;">🎃 Womens History Month 2025 <img src="Images/kiki.jpg" class="img-responsive" alt="portfolio img"></a> </li> </br> <li><a href="https://futabawebs.github.io/Articles/WhyFutabaDoesCyber" style = "text-decoration: none; color: #D43F52; font-weight: bold;">🎃 Why Futaba Does Cyber Security!? <img src="Images/BatManWL.png" class="img-responsive" alt="portfolio img"></a> </li> </br> <li><a href="https://futabawebs.github.io/Articles/FreeResourcesOrOpportunities" style = "text-decoration: none; color: #D43F52; font-weight: bold;">🎃 FREE Resource? OR Opportunities? <img src="Images/FreeResourcesOrOpportunities.jpeg" class="img-responsive" alt="portfolio img"></a> </li> </br> <li><a href="https://futabawebs.github.io/Articles/ExMachinaParlor" style = "text-decoration: none; color: #D43F52; font-weight: bold;">🎃 Ex Machina Parlor <img src="Images/ExMachinaParlor.jpeg" class="img-responsive" alt="portfolio img"></a> </li> </br> <li><a href="https://futabawebs.github.io/Articles/ConfluenceRansomwarePart1" style = "text-decoration: none; color: #D43F52; font-weight: bold;">🎃 Confluence LockBit Ransomware <img src="Images/confluence.png" class="img-responsive" alt="portfolio img"></a> </li> </br> <li><a href="https://futabawebs.github.io/Articles/ConfluenceRansomwarePart2" style = "text-decoration: none; color: #D43F52; font-weight: bold;">🎃 Confluence LockBit Ransomware (Part 2) <img src="Images/rclone.png" class="img-responsive" alt="portfolio img"></a> </li> </br> <li><a href="https://futabawebs.github.io/Articles/FutabasCyberSecurityContent" style = "text-decoration: none; color: #D43F52; font-weight: bold;">🎃 Futabas Cyber Security Content <img src="Images/FutabasCyberSecurityContent.png" class="img-responsive" alt="portfolio img"></a> </li> </br> <li><a href="https://futabawebs.github.io/Articles/Should3rdPartyVendorsBeTrusted" style = "text-decoration: none; color: #D43F52; font-weight: bold;"> 🎃 Should 3rd Party Vendors Be Trusted? <img src="Images/Should3rdPartyVendorsBeTrusted.png" class="img-responsive" alt="portfolio img"></a> </li> </br> <li><a href="https://futabawebs.github.io/Articles/TheGoalOfFutabaWebs" style = "text-decoration: none; color: #D43F52; font-weight: bold;">🎃 The Goal Of Futaba_Webs <img src="Images/TheGoalOfFutabaWebs.png" class="img-responsive" alt="portfolio img"></a> </li> </br> <li><a href="https://futabawebs.github.io/Articles/TheArtofCodeAnalysis" style = "text-decoration: none; color: #D43F52; font-weight: bold;">🎃 The Art of Code Analysis <img src="Images/TheArtofCodeAnalysis.png" class="img-responsive" alt="portfolio img"></a> </li> </br> <li><a href="https://futabawebs.github.io/Articles/CyberSecuritysBIGGESTFlaw" style = "text-decoration: none; color: #D43F52; font-weight: bold;">🎃 Cyber Securitys BIGGEST Flaw! <img src="Images/CyberSecuritysBIGGESTFlaw.jpg" class="img-responsive" alt="portfolio img"></a> </li> </br> <li><a href="https://futabawebs.github.io/Articles/TypesOfHackers" style = "text-decoration: none; color: #D43F52; font-weight: bold;">🎃 Types Of Hackers <img src="Images/TypesOfHackers.jpeg" class="img-responsive" alt="portfolio img"></a> </li> </br> <li><a href="https://futabawebs.github.io/Articles/FREEVsPaidLearning" style = "text-decoration: none; color: #D43F52; font-weight: bold;">🎃 FREE Vs PAID Learning Which is Better? <img src="Images/FREEVsPaidLearning.jpeg" class="img-responsive" alt="portfolio img"></a> </li> </br> <li><a href="https://futabawebs.github.io/Articles/SubnettingForDummies" style = "text-decoration: none; color: #D43F52; font-weight: bold;">🎃 Subnetting For Dummies <img src="Images/subnettingdiagram.png" class="img-responsive" alt="portfolio img"></a> </li> </br> <li><a href="https://futabawebs.github.io/Articles/SpecialistVsGeneralist" style = "text-decoration: none; color: #D43F52; font-weight: bold;">🎃 Specialist VS Generalist? <img src="Images/SpecialistVsGeneralist.jpeg" class="img-responsive" alt="portfolio img"></a> </li> </br> <li><a href="https://futabawebs.github.io/Articles/BondBetweenTeacherAndStudent" style = "text-decoration: none; color: #D43F52; font-weight: bold;">🎃 Bond Between Teacher And Student <img src="Images/BondBetweenTeacherAndStudent.avif" class="img-responsive" alt="portfolio img"></a> </li> </br> <li><a href="https://futabawebs.github.io/Articles/OOPForDummies" style = "text-decoration: none; color: #D43F52; font-weight: bold;">🎃 OOP For Dummies <img src="Images/OOPForDummies.png" class="img-responsive" alt="portfolio img"></a> </li> </br> <li><a href="https://futabawebs.github.io/Articles/DoCertsStillMatter" style = "text-decoration: none; color: #D43F52; font-weight: bold;">🎃 Do Certs Still Matter? <img src="Images/DoCertsStillMatter.jpg" class="img-responsive" alt="portfolio img"></a> </li> </br> <li><a href="https://futabawebs.github.io/Articles/TheEvilHERO" style = "text-decoration: none; color: #D43F52; font-weight: bold;">🎃 The Evil HERO <img src="Images/TheEvilHERO.png" class="img-responsive" alt="portfolio img"></a> </li> </br> <li><a href="https://futabawebs.github.io/Articles/TheKernel" style = "text-decoration: none; color: #D43F52; font-weight: bold;">🎃 The Kernel <img src="Images/TheKernel.jpg" class="img-responsive" alt="portfolio img"></a> </li> </br> <li><a href="https://futabawebs.github.io/Articles/TheOneAboveAll" style = "text-decoration: none; color: #D43F52; font-weight: bold;">🎃 The One Above All <img src="Images/TheOneAboveAll.jpg" class="img-responsive" alt="portfolio img"></a> </li> </br> <li><a href="https://futabawebs.github.io/Articles/CrowdStrikesFoulPlay" style = "text-decoration: none; color: #D43F52; font-weight: bold;">🎃 CrowdStrikes Foul Play <img src="Images/CrowdStrikesFoulPlay.jpg" class="img-responsive" alt="portfolio img"></a> </li> </br> <li><a href="https://futabawebs.github.io/Articles/DocumentingYourJourney" style = "text-decoration: none; color: #D43F52; font-weight: bold;">🎃 Documenting Your Journey <img src="Images/DocumentingYourJourney.jpg" class="img-responsive" alt="portfolio img"></a> </li> </br> <li><a href="https://futabawebs.github.io/Articles/BestProgrammingLanguagesForCyber" style = "text-decoration: none; color: #D43F52; font-weight: bold;">🎃 Best Programming Languages For Cyber <img src="Images/BestProgrammingLanguagesForCyber.jpg" class="img-responsive" alt="portfolio img"></a> </li> </br> <li><a href="https://futabawebs.github.io/Articles/MoreTrainingPrograms" style = "text-decoration: none; color: #D43F52; font-weight: bold;">🎃 More Training Programs <img src="Images/MoreTrainingPrograms.jpg" class="img-responsive" alt="portfolio img"></a> </li> </br> <li><a href="https://futabawebs.github.io/Articles/HowToCreateContent" style = "text-decoration: none; color: #D43F52; font-weight: bold;">🎃 How To Create Content <img src="Images/HowToCreateContent.png" class="img-responsive" alt="portfolio img"></a> </li> </br> <li><a href="https://futabawebs.github.io/Articles/TheKeyBoardInjectionFrameWork" style = "text-decoration: none; color: #D43F52; font-weight: bold;">🎃 The KeyBoard Injection FrameWork <img src="Images/TheKeyBoardInjectionFrameWork.jpg" class="img-responsive" alt="portfolio img"></a> </li> </br> <li><a href="https://futabawebs.github.io/Articles/HowtoGetMoreWomenInTech" style = "text-decoration: none; color: #D43F52; font-weight: bold;">🎃 How to Get More Women In Tech <img src="Images/HowtoGetMoreWomenInTech.png" class="img-responsive" alt="portfolio img"></a> </li> </br> </ul>';
+}
+
+//Closes the catalog list
+function ModalUnload()
+{    
+    document.getElementById("testq").innerHTML = '';
+}
+
+//This is going to set an effect for if the modal opens
+$('#flipFlop').on('show.bs.modal', function (e) {
+   
+    //Calls the ModalLoad() function that loads all the elements to the page
+    ModalLoad();
+})
+
+//This is going to set an effect for if the modal closes
+$('#flipFlop').on('hide.bs.modal', function (e) {
+   
+    //Calls the ModalLoad() function that loads all the elements to the page
+    ModalUnload();
+})
+
+
+
+
+//Music Selection
+function MusicLoad()
+{    
+    document.getElementById("musicp").innerHTML = '<p> "Shift-Alt-M" (Opens Music Selection) </br> <p> 🎃 Persona 5 (Beneath The Mask) </p> </br> <audio controls> <source src="Music/BeneaththeMask.mp3" type="audio/ogg"> <source src="Music/BeneaththeMask.mp3" type="audio/mpeg"> </audio> </br> <img src="Images/BeneathTheMask.jpg" class="img-responsive" alt="portfolio img" onclick = VelvetRoomThemeChange()> </br> <p> 🎃 Persona DSN (Whims Of Fate) </p> </br> <audio controls> <source src="Music/theWhims.mp3" type="audio/ogg"> <source src="Music/TheWhimsofFate.mp3" type="audio/mpeg"> </audio> <img src="Images/WhimsOfFate.jpg" class="img-responsive" alt="portfolio img" onclick = CrimsonDomainThemeChange()> </br>';
+}
+
+//Closes the catalog list
+function MusicUnload()
+{    
+    document.getElementById("musicz").innerHTML = '';
+}
+
+//This is going to set an effect for if the modal opens
+$('#music').on('show.bs.modal', function (e) {
+   
+    //Calls the ModalLoad() function that loads all the elements to the page
+    MusicLoad();
+})
+
+//This is going to set an effect for if the modal closes
+$('#music').on('hide.bs.modal', function (e) {
+   
+    //Calls the ModalLoad() function that loads all the elements to the page
+    MusicUnload();
+})
+
+
+//Theme Change Selection
+function ThemeChangeLoad()
+{    
+    document.getElementById("themep").innerHTML = '<p> "Shift-Alt-K" (Opens Theme Change) </br> <p> 🎃 The Velvet Room </p> </br> <img src="Images/halloweentesr.jpg" class="img-responsive" alt="portfolio img" onclick = VelvetRoomThemeChange()> </br> <p> 🎃 Crimsons Domain </p> </br> <img src="Images/Persona5red.png" class="img-responsive" alt="portfolio img" onclick = CrimsonDomainThemeChange()> </br>';
+}
+
+//Closes the catalog list
+function ThemeChangeUnload()
+{    
+    document.getElementById("themez").innerHTML = '';
+}
+
+//This is going to set an effect for if the modal opens
+$('#ThemeChange').on('show.bs.modal', function (e) {
+   
+    //Calls the ModalLoad() function that loads all the elements to the page
+    ThemeChangeLoad();
+})
+
+//This is going to set an effect for if the modal closes
+$('#ThemeChange').on('hide.bs.modal', function (e) {
+   
+    //Calls the ModalLoad() function that loads all the elements to the page
+    ThemeChangeUnload();
+})
+
+
+//Navigation Modal
+function NavigationLoad()
+{    
+    document.getElementById("testn").innerHTML = '<p> "Shift-Alt-N" (Opens Navigation Mode) </p> </br> <p> 🎃 Latest News & Updates </p> </br> <a href="https://futabawebs.github.io/Catalogs/NewsAndUpdates" style = "text-decoration: none; color: #D43F52; font-weight: bold;"> <img src="Images/apitesting.jpg" class="img-responsive" alt="portfolio img"></a></br>  <p> 🎃 Catalogs </p> </br> <a href="https://futabawebs.github.io/Catalogs/ArticleCatalog" style = "text-decoration: none; color: #D43F52; font-weight: bold;"> <img src="Images/exploitdemo.jpg" class="img-responsive" alt="portfolio img"></a></br>  <p> 🎃 Cyber Security Services </p> </br> <a href="https://futabawebs.github.io/Catalogs/Services" style = "text-decoration: none; color: #D43F52; font-weight: bold;"> <img src="Images/patreonperks.jpg" class="img-responsive" alt="portfolio img"></a></br>  <p> 🎃 Business Card </p> </br> <a href="https://linktr.ee/Futaba_Webs" style = "text-decoration: none; color: #D43F52; font-weight: bold;"> <img src="Images/maldev.jpeg" class="img-responsive" alt="portfolio img"></a></br>  <p> 🎃 Blogs </p> </br> <a href="https://futabawebs.github.io/Articles/Is_TCM_Still_Credible" style = "text-decoration: none; color: #D43F52; font-weight: bold;"> <img src="Images/malremove.jpeg" class="img-responsive" alt="portfolio img"></a></br>  <p> 🎃 Discord Server </p> </br> <a href="https://discord.gg/kHnjJ9Ew86" style = "text-decoration: none; color: #D43F52; font-weight: bold;"> <img src="Images/malresearcher.jpeg" class="img-responsive" alt="portfolio img"></a></br>  <p> 🎃 Resume </p> </br> <a href="https://futabawebs.github.io/Resume" style = "text-decoration: none; color: #D43F52; font-weight: bold;"> <img src="Images/malwareanalysis.jpg" class="img-responsive" alt="portfolio img"></a></br>  <p> 🎃 Services 2 </p> </br> <a href="https://us.cypsec.xyz/offering/futaba" style = "text-decoration: none; color: #D43F52; font-weight: bold;"> <img src="Images/malworkshop.png" class="img-responsive" alt="portfolio img"></a></br>  <p> 🎃 Terminal Mode </p> </br> <a href="https://futabawebs.github.io/TerminalMode/FullScreenMode.html" style = "text-decoration: none; color: #D43F52; font-weight: bold;"> <img src="Images/mitigationdemo.jpg" class="img-responsive" alt="portfolio img"></a></br>';
+}
+
+//Closes the catalog list
+function NavigationUnload()
+{    
+    document.getElementById("testm").innerHTML = '';
+}
+
+//This is going to set an effect for if the modal opens
+$('#navigationmenu').on('show.bs.modal', function (e) {
+   
+    //Calls the ModalLoad() function that loads all the elements to the page
+    NavigationLoad();
+})
+
+//This is going to set an effect for if the modal closes
+$('#navigationmenu').on('hide.bs.modal', function (e) {
+   
+    //Calls the ModalLoad() function that loads all the elements to the page
+    NavigationUnload();
+})
+
+
+
+<!-- Navigation modal -->
+<div class="modal fade" id="navigationmenu" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true" style = "background-image: url('');">
+	<div class="modal-dialog" role="document">
+	<div class="modal-content">
+	<div class="modal-header" style = "background-color: black;">
+
+			
+ <span class="Contact_Icons mx-4"></span>
+	<h4 class="modal-title" id="modalLabel" style = "text-align: center; font-weight: bold; font-size: 25px; color: rgb(114, 3, 193)"> Navigation Mode</h4>
+	</div>
+	<div class="modal-body">
+	
+
+		<div class = "container"> 
+			
+			<button type="button" id="contact2" class="btn btn-secondary" data-dismiss="modal" style = "background-color: black; color: rgb(114, 3, 193); border-color: rgb(114, 3, 193); font-weight: bold;">🕸🎃🕸 CLOSE 🕸🎃🕸</button>
+			
+			<a href="https://futabawebs.github.io/"> <button type="button" id="contact2" class="btn btn-secondary" style = "text-decoration: none; background-color: black; color: rgb(114, 3, 193); border-color: rgb(114, 3, 193); font-weight: bold;">🕸🎃🕸 Home 🕸🎃🕸</button> </a>
+
+			<button type="button" onclick = Navigation() id="navi" class="btn btn-secondary" style = "text-decoration: none; background-color: black; color: rgb(114, 3, 193); border-color: rgb(114, 3, 193); font-weight: bold;">🕸🎃🕸 Navigation 🕸🎃🕸</button> 
+		
+		</div>
+
+
+		<script src = "test.js"></script>
+		
+
+	</div>
+	<div class="modal-footer" style = "background-color: black; color: rgb(114, 3, 193); text-align: center; font-weight: bold;">
+
+		<p id = "testn"></p>
+		<p id = "testm"></p>
+
+	</br>
+</br>
+
+
+	</div>
+	</div>
+	</div>
+	</div>
+
+
+
+<!-- ThemeChange modal -->
+<div class="modal fade" id="ThemeChange" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true" style = "background-image: url('');">
+	<div class="modal-dialog" role="document">
+	<div class="modal-content">
+	<div class="modal-header" style = "background-color: black;">
+
+			
+ <span class="Contact_Icons mx-4"></span>
+	<h4 class="modal-title" id="modalLabel" style = "text-align: center; font-weight: bold; font-size: 25px; color: rgb(114, 3, 193)"> Theme Change</h4>
+	</div>
+	<div class="modal-body">
+	
+
+		<div class = "container"> 
+			
+			<button type="button" id="contact2" class="btn btn-secondary" data-dismiss="modal" style = "background-color: black; color: rgb(114, 3, 193); border-color: rgb(114, 3, 193); font-weight: bold;">🕸🎃🕸 CLOSE 🕸🎃🕸</button>
+			
+			<a href="https://futabawebs.github.io/"> <button type="button" id="contact2" class="btn btn-secondary" style = "text-decoration: none; background-color: black; color: rgb(114, 3, 193); border-color: rgb(114, 3, 193); font-weight: bold;">🕸🎃🕸 Home 🕸🎃🕸</button> </a>
+
+			<button type="button" onclick = Navigation() id="navi" class="btn btn-secondary" style = "text-decoration: none; background-color: black; color: rgb(114, 3, 193); border-color: rgb(114, 3, 193); font-weight: bold;">🕸🎃🕸 Navigation 🕸🎃🕸</button> 
+
+			<a hidden href="https://futabawebs.github.io/"> <button type="button" onclick = Navigation() id="navi2" class="btn btn-secondary" style = "text-decoration: none; background-color: black; color: rgb(114, 3, 193); border-color: rgb(114, 3, 193); font-weight: bold;">🕸🎃🕸 Navigation Close🕸🎃🕸</button> </a>
+		
+		</div>
+
+
+		<script src = "test.js"></script>
+		
+
+	</div>
+	<div class="modal-footer" style = "background-color: black; color: rgb(114, 3, 193); text-align: center; font-weight: bold;">
+
+		<p id = "themep"></p>
+		<p id = "themez"></p>
+
+
+
+	</div>
+	</div>
+	</div>
+	</div>
+
+
+<!-- Music modal -->
+<div class="modal fade" id="music" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true" style = "background-image: url('');">
+	<div class="modal-dialog" role="document">
+	<div class="modal-content">
+	<div class="modal-header" style = "background-color: black;">
+
+			
+ <span class="Contact_Icons mx-4"></span>
+	<h4 class="modal-title" id="modalLabel" style = "text-align: center; font-weight: bold; font-size: 25px; color: rgb(114, 3, 193)"> Music Mode</h4>
+	</div>
+	<div class="modal-body">
+	
+
+		<div class = "container"> 
+			
+			<button type="button" id="contact2" class="btn btn-secondary" data-dismiss="modal" style = "background-color: black; color: rgb(114, 3, 193); border-color: rgb(114, 3, 193); font-weight: bold;">🕸🎃🕸 CLOSE 🕸🎃🕸</button>
+			
+			<a href="https://futabawebs.github.io/"> <button type="button" id="contact2" class="btn btn-secondary" style = "text-decoration: none; background-color: black; color: rgb(114, 3, 193); border-color: rgb(114, 3, 193); font-weight: bold;">🕸🎃🕸 Home 🕸🎃🕸</button> </a>
+
+			<button type="button" onclick = Navigation() id="navi" class="btn btn-secondary" style = "text-decoration: none; background-color: black; color: rgb(114, 3, 193); border-color: rgb(114, 3, 193); font-weight: bold;">🕸🎃🕸 Navigation 🕸🎃🕸</button> 
+
+			<a hidden href="https://futabawebs.github.io/"> <button type="button" onclick = Navigation() id="navi2" class="btn btn-secondary" style = "text-decoration: none; background-color: black; color: rgb(114, 3, 193); border-color: rgb(114, 3, 193); font-weight: bold;">🕸🎃🕸 Navigation Close🕸🎃🕸</button> </a>
+		
+		</div>
+
+
+		<script src = "test.js"> </script>
+		
+
+	</div>
+	<div class="modal-footer" style = "background-color: black; color: rgb(114, 3, 193); text-align: center; font-weight: bold;">
+
+		<p id = "musicp"></p>
+		<p id = "musicz"></p>
+
+	</br>
+</br>
+
+
+	</div>
+	</div>
+	</div>
+	</div>
